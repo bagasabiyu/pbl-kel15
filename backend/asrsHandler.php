1965cb6643de7d4b0285a1cb7cdfb0bd701c694c
IsaacRK <auxbot4@gmail.com>
2021-06-18 17:33:03 +0700

    revisi input database table log

--- backend/asrsHandler.php
+++ backend/asrsHandler.php
@@ -23,7 +23,7 @@ if($err == 0)
 	$query = "UPDATE rak SET status = '$stat' WHERE adress = '$add'";
 	mysqli_query($conn, $query);
 }
-
+//input ke tabel log
 if (isset($_POST['inputAdress'])){
 	$query = "SELECT * FROM log WHERE id = '1'";
 	$run = mysqli_query($conn, $query);
@@ -31,7 +31,7 @@ if (isset($_POST['inputAdress'])){
 		$query = "UPDATE log SET adress = '$add', status = '$stat' WHERE id = '1'";
 		$run = mysqli_query($conn, $query);
 	}else{
-		$query = "INSERT INTO log (adress ,status) VALUES ('$add' ,'$stat')";
+		$query = "INSERT INTO log (adress ,status) VALUES (DEFAULT,'$add' ,'$stat')";
 		$run = mysqli_query($conn, $query);
 	}
 }
